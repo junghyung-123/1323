e1ea6a2448e2c5b836b9afd12695587e57241972
pc-06 <pc-06@pc-06-PC>
2017-10-18 14:14:20 +0900

    수정

--- GIT_test/src/Main.java
+++ GIT_test/src/Main.java
@@ -3,8 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("rrrrrrrrr"); 
-		
-
+		System.out.println("123f");
+		System.out.println("123f4");		
+		System.out.println("123f123123");
 	}
 
 }
